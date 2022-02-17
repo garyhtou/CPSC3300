dace93d0219bfdcf80c4717ba2c6ee01ca3ec658
Gary Tou <gary@garytou.com>
2022-02-16 22:07:45 -0800

    Clean menu item name

--- pda4/pda4.py
+++ pda4/pda4.py
@@ -4,15 +4,22 @@ import random
 from faker import Faker
 
 generate = Faker()
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
 NULL = "NULL"
+# Current path for file
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
+"""
+Helper function to wrap in quotes
+"""
 def wrapStr(s): return "\"" + str(s) + "\""
 
 
 class McDonaldsMenu:
+    """
+    Used to access the McDonalds menu item data set. `menu.csv` must be
+    present in the same directory as this file.
+    """
+
     def __init__(self):
         self.products = []
 
@@ -27,16 +34,34 @@ class McDonaldsMenu:
                         continue
 
                     data = line.split(",")
+
                     name = data[1]
                     calories = data[3]
+
+                    # Remove leading and trailing quotes if they exist
+                    if name.startswith('"') and name.endswith('"'):
+                        name = name[1:-1]
+
+                    # Remove non-ascii characters
+                    name = name.encode("ascii", "ignore").decode("ascii")
 
                     self.products.append((name, calories))
 
         except FileNotFoundError:
-            print("No menu file found, please include `menu.csv` from https://www.kaggle.com/mcdonalds/nutrition-facts")
+            print("No menu file found, please place `menu.csv` from"
+                  "https://www.kaggle.com/mcdonalds/nutrition-facts in the same"
+                  "directory as this python script.")
             exit(1)
 
     def randItem(self):
+        """
+        Return a random unique menu item. If all menu items have been used, it
+        will return None.
+
+        Returns:
+            (string, int) | None: Name and calories of the current menu item
+        """
+        # Return none if we've run out of items
         if len(self.products) == 0:
             return None
 
@@ -44,10 +69,17 @@ class McDonaldsMenu:
         return self.products.pop(random.randint(0, len(self.products) - 1))
 
 
+# An instance of the McDonalds menu
+menu = McDonaldsMenu()
+
+# List of used phone numbers by `randPhoneE164`
 usedPhones = []
 
 
 def randPhoneE164():
+    """
+    Generate a random phone number in E.164 format.
+    """
     def series(size): return "".join(
         [str(random.randint(0, 9)) for _ in range(size)])
 
@@ -63,7 +95,10 @@ def randPhoneE164():
 
 
 class Relation(object):
-    id = 0
+    """
+    Base class for all relations
+    """
+    id = 0  # Current id (autoincrement)
     config = {}
     associations = []
     ID_ATTRIBUTE = 'id'
@@ -99,27 +134,41 @@ class Relation(object):
 
     @classmethod
     def header(cls):
-        # Get attribute names (add id) and join with commas
+        """
+        Returns the CSV header for the relation
+
+        Returns:
+            string: CSV header for the relation
+        """
+        # Get attribute names (add id)
         attributes = [cls.ID_ATTRIBUTE]+list(cls.config.keys())
+        # Get foreign key attribute names
         relations = [relation.fkAttributeName()
                      for relation in cls.associations]
+        # Join with commas
         return ','.join(attributes + relations)
 
     @classmethod
-    def fkAttributeName(
-        cls): return f'{cls.__name__.lower()}_{cls.ID_ATTRIBUTE}'
+    def fkAttributeName(cls):
+        """
+        Return the column attribute name for when this relation is referenced as
+        a foreign key
+
+        Returns:
+            string: column attribute name
+        """
+        return f'{cls.__name__.lower()}_{cls.ID_ATTRIBUTE}'
 
 
 class Customer(Relation):
     config = {
         "name": lambda: wrapStr(generate.unique.name()),
-        "address": lambda: wrapStr(generate.address().replace('\n', ' ')),
-        "phone": lambda: wrapStr(random.choices([randPhoneE164(), NULL], weights=[90, 10])[0]),
+        "phone": lambda: wrapStr(random.choices(
+            [randPhoneE164(), NULL],
+            weights=[90, 10]  # 10% chance of NULL
+        )[0]),
         "email": lambda: wrapStr(generate.unique.email()),
     }
-
-
-menu = McDonaldsMenu()
 
 
 class Item(Relation):
@@ -127,24 +176,44 @@ class Item(Relation):
 
     @staticmethod
     def newRandMenuItem(cls):
+        """
+        Assign a new random menu item to the class. This is a janky way to make
+        sure that `randItem` and `randCalories` return data that belong to the
+        same menu item (until the menu is exhausted this method is called
+        again).
+        """
         cls.menuItem = menu.randItem()
         return cls.menuItem
 
     @classmethod
     def randName(cls):
+        """
+        Returns the name of the current random unique menu item
+
+        Returns:
+            string: The name of a menu item
+        """
         name, _ = cls.menuItem
         return name
 
     @classmethod
     def randCalories(cls):
+        """
+        Returns the calories of the current random unique menu item
+
+        Returns:
+            string: The calories of a menu item
+        """
         _, calories = cls.menuItem
         return calories
 
     config = {
         "name": lambda: wrapStr(Item.randName()),
         "price_cents": lambda: random.randint(1, 15_00),
-        # "calories": lambda: random.choices([random.randint(20, 2000), NULL], weights=[90, 10])[0],
-        "calories": lambda: random.choices([Item.randCalories(), NULL], weights=[90, 10])[0],
+        "calories": lambda: random.choices(
+            [Item.randCalories(), NULL],
+            weights=[90, 10]  # 10% chance of NULL
+        )[0],
     }
 
 
@@ -162,6 +231,7 @@ class Order(Relation):
     config = {
         "date": generate.date_time,
     }
+    # Order has a foreign key
     associations = [Customer, Store]
 
 
